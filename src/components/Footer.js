72004d2f8e346d93bd91e0899b68a80ca31f0428
Adriano César Gotardo <adrianogotardo.12@gmail.com>
2021-08-27 11:32:23 -0300

    Fixed the order generation at the footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,50 @@
 import styled from 'styled-components';
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 
 import CartContext from '../contexts/CartContext';
 
 export default function Footer() {
     const { meals, drinks, desserts } = useContext(CartContext);
+    const [mealSelected, setMealSelected] = useState(false);
+    const [drinkSelected, setDrinkSelected] = useState(false);
+    const [dessertSelected, setDessertSelected] = useState(false);
+    const [finalOrder, setFinalOrder] = useState([]);
+
+    useEffect(() => {
+        let order = [];
+        setMealSelected(false);
+        setDrinkSelected(false);
+        setDessertSelected(false);
+        meals.forEach(meal => { if(meal.amount > 0) {
+            setMealSelected(true);
+            order.push(meal);
+        }})
+        drinks.forEach(drink => {if(drink.amount > 0) {
+            setDrinkSelected(true);
+            order.push(drink);
+        }})
+        desserts.forEach(dessert => {if(dessert.amount > 0) {
+            setDessertSelected(true);
+            order.push(dessert);
+        }})
+        setFinalOrder(order);
+    }, [meals, drinks, desserts]);
+
+    console.log(finalOrder);
     
     function orderProduct() {
-        // generates order
-        let selectedItems = [];
-        meals.forEach(meal => {if(meal.amount > 0) selectedItems.push(meal)})
-        drinks.forEach(drink => {if(drink.amount > 0) selectedItems.push(drink)})
-        desserts.forEach(dessert => {if(dessert.amount > 0) selectedItems.push(dessert)})
-        console.log(selectedItems);
+        const names = finalOrder.map(m => {
+            return m.name;
+        })
+        alert("Seu pedido: " + names);
     }
 
     return(
         <FooterBox>
-            <Button disabled={true} onClick={() => orderProduct()}>
-                <h1>Selecione os 3 itens para fechar o pedido</h1>
+            <Button disabled={!mealSelected || !drinkSelected || !dessertSelected} onClick={() => orderProduct()}>
+                {mealSelected && drinkSelected && dessertSelected
+                ? <h1>Realizar pedido</h1>
+                : <h1>Selecione os 3 itens para fechar o pedido</h1>}
             </Button>
         </FooterBox>
     )
@@ -42,10 +68,10 @@ const Button = styled.button`
     width: 80%;
     height: 61px;
     padding: 0px 40px;
-    background-color: #CBCBCB;
+    background-color: ${props => props.disabled ? "#CBCBCB" : "#32B72F"};
     border: none;
     border-radius: 50px;
-    cursor: pointer;
+    cursor: ${props => props.disabled ? "auto" : "pointer"};
     h1 {
         font-family: Roboto;
         font-size: 20px;
